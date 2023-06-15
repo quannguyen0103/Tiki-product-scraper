ec7a372831aa6acda8c8dca2f47b5439c6f9a6f2
Nguyen Quan <108479982+quannguyen0103@users.noreply.github.com>
2023-06-15 10:01:06 +0700

    Update extract_data.py

--- src/extract_data.py
+++ src/extract_data.py
@@ -6,31 +6,34 @@ import pymongo
 import re
 import csv
 
-# Get ingredient data and insert to a CSV file
+# Defind a function to extract ingredient data
+def extract_ingredient(collection):
+    pattern = re.compile("thành phần:", re.IGNORECASE)
+    query = {"description": {"$regex": pattern}}
+    mongo_documents = collection.find(query)
+
+    for document in mongo_documents:
+        product_id = document.get("id")
+        soup = BeautifulSoup(document["description"], "html.parser")
+        description = []
+        for tag in soup.find_all():
+            if tag.string:
+                description.append(tag.string.strip("- "))
+        for desc in description:
+            if 'thành phần:' in desc.lower():
+                ingredient = desc.lower().find('thành phần:')
+                if ingredient != -1:
+                    ingredient = "thành phần: " + desc[ingredient + len('thành phần:'):].strip()
+        if ingredient.strip() == "thành phần:":
+            continue
+        else:
+            data = str(product_id) + ', ' + ingredient.rstrip(".")
+            with open("ingredient.csv", "a", newline="", encoding="utf-16") as f:
+                writer = csv.writer(f)
+                writer.writerow([data])
+            print(f"product {product_id} written to ingredient.csv successfully.")
+
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["tiki_product"]
 collection = db["tiki_data"]
-pattern = re.compile("thành phần:", re.IGNORECASE)
-query = {"description": {"$regex": pattern}}
-mongo_documents = collection.find(query)
-
-for document in mongo_documents:
-    product_id = document.get("id")
-    soup = BeautifulSoup(document["description"], "html.parser")
-    description = []
-    for tag in soup.find_all():
-        if tag.string:
-            description.append(tag.string.strip("- "))
-    for desc in description:
-        if 'thành phần:' in desc.lower():
-            ingredient = desc.lower().find('thành phần:')
-            if ingredient != -1:
-                ingredient = "thành phần: " + desc[ingredient + len('thành phần:'):].strip()
-    if ingredient.strip() == "thành phần:":
-        continue
-    else:
-        data = str(product_id) + ', ' + ingredient.rstrip(".")
-        with open("ingredient.csv", "a", newline="", encoding="utf-16") as f:
-            writer = csv.writer(f)
-            writer.writerow([data])
-        print(f"product {product_id} written to ingredient.csv successfully.")
+extract_ingredient(collection)
